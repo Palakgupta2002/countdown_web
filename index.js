fa476b8cba89340cc955f14f684eb719b1449fed
Palakgupta2002 <pg948303@gmail.com>
2023-04-01 15:53:53 +0530

    DiGITAL CLOCK

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const ampm = document.getElementById("ampm");
+function getTime(){
+  
+   
+    hour.innerText=new Date().getHours();
+    
+    minute.innerText=new Date().getMinutes();
+   
+    second.innerText=new Date().getSeconds();
+    setTimeout(()=>{
+        getTime()
+    },1000)
+}
+getTime();
 
-var dt=new Date();
-var day=document.getElementById("day");
-day.innertext=dt.getDate();
+
+
+
